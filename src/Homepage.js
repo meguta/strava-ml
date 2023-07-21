ac2fa7715f73d5dd596def230b46901054421af3
Ahmed Imana <ahmedoy@icloud.com>
2023-07-20 20:08:02 -0400

    initial commit

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import {Text, Heading, Flex} from "@chakra-ui/react"
+
+function Homepage() {
+    return ( 
+        <Flex>
+            <Heading>
+                TheBike
+            </Heading>
+        </Flex>
+    )
+}
+
+export default Homepage
